1ff008a866dd94831c122ddacf915208570f3f80
Jonathan Ta <TeaOfJay@users.noreply.github.com>
2020-12-02 00:17:20 -0800

    commit for evaluating timing and convergence

--- sokoban.py
+++ sokoban.py
@@ -10,6 +10,7 @@ import agent
 from agent import QAgent
 from environment import Environment
 from environment import MapType
+import time
 
 # def debug_print(fmt_string):
 # 	print(fmt_string)
@@ -56,49 +57,110 @@ def main():
 				player = np.array([int(row[0]), int(row[1])])
 
 	environment = Environment(walls = walls, boxes = boxes, storage = storage, player = player, xlim = xlim, ylim = ylim)
-	agent = QAgent(environment = environment, learning_rate = 1., discount_factor=0.9, replay_rate = 0.2)
+	# agent = QAgent(environment = environment, learning_rate = 1., discount_factor=0.9, replay_rate = 0.2)
 
-	episode_bookmarks = []
-	episode_iterations = []
+	def converge(evaluations):
+		if len(evaluations) >= 2 and evaluations[-2] - evaluations[-1] == 0 and evaluations[-1] < 10:
+			return True
+		else:
+			return False
 
-	num_episodes = 0
-	num_iterations = 0
-	goals_reached = 0
-	while num_episodes < max_episodes:
-		goal, iterations = agent.episode(draw = args.draw)
+	replay_rates = [1.0, 0.9, 0.7, 0.5, 0.3, 0.2]
+	data = []
+	for rate in replay_rates:
+		print(rate)
+		#for i in range(10):
+			#if i % 10 == 0:
 
-		if goal:
-			goals_reached += 1
-			episode_bookmarks.append(num_episodes)
-			episode_iterations.append(iterations)
-			print(f"{num_episodes:5d}:goal reached.")
-		if num_episodes % 100 == 0:
-			print(f"{num_episodes:5d}:")
+		#print(f"iteration {i}:")
+		start_time = time.process_time()
+		agent = QAgent(environment = environment, learning_rate = 1., discount_factor=0.9, replay_rate = rate)
 
+		num_episodes = 0
+		num_iterations = 0
+		goals_reached = 0
 
-		if num_episodes > 0 and num_episodes % 100 == 0:
-			goal, iterations = agent.episode(draw = True, evaluate=True, max_iterations = 200)
-			print("-"*20)
-			print(f"evaluation:{goal}")
+		evaluations = []
+		goal_reached = False
+		while (num_episodes < max_episodes and not (goal_reached and converge(evaluations))):
+			goal, iterations = agent.episode(draw = args.draw)
+
 			if goal:
-				print(f"iterations:{iterations}")
-			print("-"*20)
+				goals_reached += 1
+				#print(f"{num_episodes:5d}:goal reached.")
+			if num_episodes % 100 == 0:
+				#pass
+				print(f"{num_episodes:5d}:")
 
 
-		num_episodes += 1
-	episode_iterations = np.array(episode_iterations)
+			if num_episodes > 0 and num_episodes % 100 == 0:
+				goal_reached, iterations = agent.episode(draw = False, evaluate=True, max_iterations = 200)
+				print("-"*20)
+				print(f"evaluation:{goal_reached}")
+				if goal_reached:
+					print(f"iterations:{iterations}")
+				print("-"*20)
+				evaluations.append(iterations)
+			num_episodes += 1
 
-	goal, iterations = agent.episode(draw = True, evaluate=True, max_iterations = 200)
+		goal, iterations = agent.episode(draw = False, evaluate=True, max_iterations = 200)
 
-	print("-"*30)
-	print("Simulation ended.")
-	print(f"episodes   :{num_episodes}")
-	print(f"map solved :{goal}")
-	print(f"iterations :{iterations}")
+		data.append((rate, time.process_time() - start_time, iterations))
+	# 		xs.append(rate)
+	# 		ys.append(time.process_time()-start_time)
+
+	# plt.scatter(xs, ys)
+
+	# plt.show(block=True)
+	# plt.savefig('times.png')
+
+	with open('times.csv', 'a') as f:
+		writer = csv.writer(f)
+
+		for item in data:
+			writer.writerow(item)
+
+
+
+
+	# num_episodes = 0
+	# num_iterations = 0
+	# goals_reached = 0
+
+	# evaluations = []
+	# goal_reached = False
+	# while (num_episodes < max_episodes and not (goal_reached and converge(evaluations))):
+	# 	goal, iterations = agent.episode(draw = args.draw)
+
+	# 	if goal:
+	# 		goals_reached += 1
+	# 		print(f"{num_episodes:5d}:goal reached.")
+	# 	if num_episodes % 100 == 0:
+	# 		print(f"{num_episodes:5d}:")
+
+
+	# 	if num_episodes > 0 and num_episodes % 100 == 0:
+	# 		goal_reached, iterations = agent.episode(draw = False, evaluate=True, max_iterations = 200)
+	# 		print("-"*20)
+	# 		print(f"evaluation:{goal_reached}")
+	# 		if goal_reached:
+	# 			print(f"iterations:{iterations}")
+	# 		print("-"*20)
+	# 		evaluations.append(iterations)
+
+
+	# 	num_episodes += 1
+	# episode_iterations = np.array(episode_iterations)
+
+	# goal, iterations = agent.episode(draw = True, evaluate=True, max_iterations = 200)
+
+	# print("-"*30)
+	# print("Simulation ended.")
+	# print(f"episodes   :{num_episodes}")
+	# print(f"map solved :{goal}")
+	# print(f"iterations :{iterations}")
 	
 
-
-	plt.show(block=True)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Solve a Sokoban game using artificial intelligence.")
